5806bd994249fe181195868f8d0cc20bf5daa89e
Satyam Rathi <satyam.rathi47@gmail.com>
2025-05-28 13:14:03 +0530

    got the human choice

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let getComputerChoice = function(){
     let choice = Math.floor(Math.random() * choices.length);
     return choices[choice];
 }
+let getHumanChoice = function(){
+    let hChoice = prompt("enter your choice :");
+    return hChoice;
+}
+
+document.getElementById("rock").onclick = () => playRound("rock");
+document.getElementById("paper").onclick = () => playRound("paper");
+document.getElementById("scissors").onclick = () => playRound("scissors");
