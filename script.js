675a7873c4947d25f36298c92f00b75251e75b83
Satyam Rathi <satyam.rathi47@gmail.com>
2025-05-28 12:44:33 +0530

    got the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Rock Paper Scissors Game Loaded");
+let getComputerChoice = function(){
+    let choices = ["rock", "paper", "scissors"];
+    let choice = Math.floor(Math.random() * choices.length);
+    return choices[choice];
+}
